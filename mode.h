6174bec3da024ee1ea1b970b4e8618c4ae4f3734
TunaRepublic <127034158+TunaRepublic@users.noreply.github.com>
2023-03-09 22:57:52 +0100

    Update mode.h

--- mode.h
+++ mode.h
@@ -1,2 +1,5 @@
 typedef enum { PRACTICE, TEST } Mode;
 Mode mode;
+bool case_sens;
+bool low_upp_case; // 0 if lowecase; 1 if uppercase
+bool low_upp_both; // 1 if both lowercase and uppercase; 0 otherwise
